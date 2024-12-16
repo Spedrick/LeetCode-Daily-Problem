ebc738caaaba0b7bf10bccffbbc23a8df959b056
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-12-16 14:17:52 +0530

    [3264] [Time Beats: 20.56%] [Memory Beats: 20.77%] - LeetPush

---
+++ DCP-12-24/3264-Final-Array-State-After-K-Multiplication-Operations-I.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for(int i=0;i<nums.size();i++) pq.push({nums[i], i});
+
+        while(k--) {
+            int x = pq.top().first;
+            int idx = pq.top().second;
+
+            pq.pop();
+            pq.push({x*multiplier, idx});
+        }
+
+        while(!pq.empty()) {
+            nums[pq.top().second] = pq.top().first;
+            pq.pop();
+        }
+
+        return nums;
+    }
+};
